44df8b7b24390a392e14b0cf00d561dd9e24189e
Sanaya James <sanaya.j.james@gmail.com>
2025-11-06 13:09:48 -0700

    Completed lesson

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,17 @@
 import React from "react";
+import NavBar from './NavBar';
+import About from "./About";
+import Home from "./Home";
+
+import user from "../data/user";
+console.log(user);
 
 function App() {
   return (
     <div>
-      {/* add the <Navbar> component here */}
-      {/* add the <Home> component here */}
-      {/* add your <About> component here */}
+      <NavBar />
+      <Home name={user.name} city={user.city} color={user.color} />
+      <About bio={user.bio} links={user.links} />
     </div>
   );
 }
---
+++ src/components/Links.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Links({ github, linkedin }) {
+  console.log(Links)
+  return (
+
+    <div id="links">
+      <h3>Links </h3>
+      <a href={github}>{github}</a>
+      <a href={linkedin}>{linkedin}</a>
+    </div>
+  );
+}
+
+export default Links;
